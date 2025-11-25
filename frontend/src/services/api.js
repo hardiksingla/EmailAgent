5450e06d1529685ca8eaef3947ba13181ad0ad89
hardiksingla <07hardiksingla@gmail.com>
2025-11-26 01:14:25 +0530

    fix:deployment issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  // baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://emailagent-95h4.onrender.com/',
 });
 
 export const fetchEmails = async () => {
