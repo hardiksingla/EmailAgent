54ce8ece1c6c9c85caddb01505c0ba2c160c69a1
hardiksingla <07hardiksingla@gmail.com>
2025-11-26 01:08:26 +0530

    fix:deployment issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:5001/api',
-// });
+const api = axios.create({
+  baseURL: 'http://localhost:5001/api',
+});
 
 export const fetchEmails = async () => {
   const response = await api.get('/emails'); 
